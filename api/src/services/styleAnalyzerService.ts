abc136f1d0ffc55cb32f926e1f2d423bc30270d8
EddyDezuraud <eddy.dezuraud@gmail.com>
2024-04-28 21:57:33 +0200

    Add puppeteer for web scraping

--- api/src/services/styleAnalyzerService.ts
+++ api/src/services/styleAnalyzerService.ts
@@ -1,10 +1,15 @@
 import { StylesConfig } from '../types/StylesConfig';
-
+import puppeteer, {Page, ElementHandle} from 'puppeteer';
 import { extractMultipleTagStyles } from '../helpers/utils';
 
 const styleAnalyser = async (url: URL): Promise<StylesConfig> => {
 
-    const computedButtons:CSSStyleDeclaration[] = extractMultipleTagStyles(["button","a"]); // Array of all buttons a div with class button
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url.toString());
+
+
+    const computedButtons:CSSStyleDeclaration[] = extractMultipleTagStyles(page, ["button","a"]); // Array of all buttons a div with class button
 
     // define primary and secondary buttons
 
