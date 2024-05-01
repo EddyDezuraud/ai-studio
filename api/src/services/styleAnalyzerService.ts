4e73a792b9aeef5094ab05aa4bed1fa5090ca983
EddyDezuraud <eddy.dezuraud@gmail.com>
2024-05-01 14:44:44 +0200

    Add primary and secondary button definitions

--- api/src/services/styleAnalyzerService.ts
+++ api/src/services/styleAnalyzerService.ts
@@ -12,6 +12,8 @@ const styleAnalyser = async (url: URL): Promise<StylesConfig> => {
     const computedButtons:CSSStyleDeclaration[] = extractMultipleTagStyles(page, ["button","a"]); // Array of all buttons a div with class button
 
     // define primary and secondary buttons
+    
+    
 
     return {} as StylesConfig;
 }
