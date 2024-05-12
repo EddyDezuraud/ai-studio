6fdca4d1c051883093778548c1256f45945a3fe5
Eddy Dezuraud <eddy@gedivote.fr>
2024-05-12 22:01:15 +0200

    Refactor websiteService to accept cookies before taking screenshots

--- api/src/services/websiteService.ts
+++ api/src/services/websiteService.ts
@@ -1,6 +1,38 @@
 import { Screenshots } from '../types/StylesConfig'
 import puppeteer from "puppeteer-extra"
+import { Page } from "puppeteer"
 import stealth from "puppeteer-extra-plugin-stealth"
+
+const cookieKeywords = ['accept', 'ok', 'valider', 'validate', 'agree', 'compris', 'comprendre', 'accepter'];
+
+const acceptCookies = async (page: Page) => {
+    // await page.waitForNavigation();
+    await page.waitForSelector('body', { timeout: 60000 });
+
+    // array of all buttons
+    const buttons = await page.$$('button');
+
+    // find the button that contains the keyword
+    const acceptButton = buttons.find(async (button) => {
+        const buttonText = await button.evaluate((node) => node.textContent);
+        return cookieKeywords.some(keyword => buttonText?.toLowerCase().includes(keyword));
+    });
+
+    // click the button
+    if(acceptButton) {
+
+        // acceptButton classname
+        const acceptButtonClass = await acceptButton.evaluate((node) => node.className);
+        console.log('acceptButtonClass', acceptButtonClass);
+
+        await page.waitForSelector('button', { timeout: 60000 });
+
+        await acceptButton?.click();
+        await page.waitForNavigation({
+            waitUntil: 'networkidle0',
+        });
+    }
+  };
 
 const getScreenshots = async (url: string): Promise<Screenshots> => {
 
@@ -20,6 +52,7 @@ const getScreenshots = async (url: string): Promise<Screenshots> => {
 
     const page = await browser.newPage();
     await page.goto(url);
+    await acceptCookies(page);
 
     await page.setViewport({width:1920, height:1080});
 
